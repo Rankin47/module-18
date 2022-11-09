6489d183915f34858d2a99d8c3e6e150e5b426b6
Rankin47 <108376982+Rankin47@users.noreply.github.com>
2022-11-09 15:44:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+// const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+// app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/api', {
+
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
+app.get("/users", (req, res) => {
+  res.send("sup");
+});
 
-mongoose.set('debug', true);
-
+// mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
