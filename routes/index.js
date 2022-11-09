cf0f0924969e0fc34968db1dfd53bb2e276b5e68
Rankin47 <108376982+Rankin47@users.noreply.github.com>
2022-11-09 15:44:43 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
+const apiRoutes = require("./api");
+router.use("/", (req, res) => {
+  res.send("hello");
+});
+router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('<h1>404 Error</h1>');
+  res.status(404).send("<h1>404 Error</h1>");
 });
 
 module.exports = router;
